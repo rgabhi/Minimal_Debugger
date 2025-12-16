b790dd77e98b69037ea2322f3c506854f756e3e3
pratikchaudhari13052003 <pratikchaudhari13052003@gmail.com>
2025-12-16 18:30:33 +0530

    add: break,remove breakpoint,inspect register

--- breakpoint.hpp
+++ breakpoint.hpp
@@ -1,1 +1,27 @@
-// todo - pratik
+
+
+
+#ifndef BREAKPOINT_HPP
+#define BREAKPOINT_HPP
+
+#include <sys/types.h> // pid_t
+#include <cstdint> // std::intptr_t, uint8_t
+
+class BreakPoint {
+    public:
+        BreakPoint(pid_t pid, std::intptr_t addr);
+
+        void enable();
+        void disable();
+
+        bool is_enabled() const;
+        std::intptr_t get_address() const;
+    private:
+        pid_t m_pid;
+        std::intptr_t m_addr;
+        bool m_enabled;
+        uint8_t m_saved_data; // to store og byte
+
+};
+#endif
+
--- debugger.cpp
+++ debugger.cpp
@@ -10,8 +10,6 @@
 using namespace std;
 
 
-    } else {
-
 int main(int argc, char* argv[]) {
     if(argc < 2){
         std::cerr << "Usage: " <<argv[0] << "[program]" << endl;
